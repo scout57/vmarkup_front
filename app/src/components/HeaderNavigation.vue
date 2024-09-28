1b9f675f9c5a0e44ad69e4ab4e49bafcb13ca77e
Алексей Король <a.korol@applegalhelp.ru>
2024-09-28 03:40:39 +0300

    v2

---
+++ app/src/components/HeaderNavigation.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-tabs fixed-tabs>
+    <v-tab prepend-icon="mdi-text-search" to="/video/create">Список</v-tab>
+    <v-tab prepend-icon="mdi-video-plus" to="/video/create">Создание</v-tab>
+    <v-tab prepend-icon="mdi-file-video-outline" to="/video/create">Карточка</v-tab>
+  </v-tabs>
+</template>
+<script>
+export default {
+  data: () => ({
+    tab: null,
+  }),
+}
+</script>
--- app/src/pages/video.create.vue
+++ app/src/pages/video.create.vue
@@ -1,39 +1,67 @@
 <template>
-  <v-sheet class="bg-deep-purple pa-12 h-screen" rounded>
-    <v-card class="mx-auto px-6 py-6" max-width="600" title="Новое видео">
-      <v-form
-        v-model="form"
-        @submit.prevent="onSubmit"
-      >
+  <v-responsive class="border rounded">
+    <v-app :theme="theme">
+      <v-app-bar class="pr-4"  title="GPT Enjoyers" >
 
-        <v-file-input
-          class="py-6"
-          label="Файл"
-          show-size
-          v-model="file"
-          variant="outlined"
-        ></v-file-input>
+        <v-spacer></v-spacer>
+        <header-navigation/>
 
+        <v-spacer></v-spacer>
         <v-btn
-          :disabled="!form"
-          :loading="loading"
-          color="success"
-          size="large"
-          type="submit"
-          variant="elevated"
-          block
-        >
-          Загрузить
-        </v-btn>
-      </v-form>
-    </v-card>
-  </v-sheet>
+          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          :text="theme === 'light' ? 'Светлая' : 'Темная'"
+          slim
+          @click="onClick"
+        ></v-btn>
+      </v-app-bar>
+
+      <v-main>
+        <v-container>
+          <v-card class="mx-auto px-6 py-6" max-width="600" title="Новое видео">
+            <v-form
+              v-model="form"
+              @submit.prevent="onSubmit"
+            >
+
+              <v-file-input
+                class="py-6"
+                label="Файл"
+                show-size
+                v-model="file"
+                variant="outlined"
+              ></v-file-input>
+
+              <v-btn
+                :disabled="!form"
+                :loading="loading"
+                color="success"
+                size="large"
+                type="submit"
+                variant="elevated"
+                block
+              >
+                Загрузить
+              </v-btn>
+            </v-form>
+          </v-card>
+        </v-container>
+      </v-main>
+    </v-app>
+  </v-responsive>
 </template>
 
 
 <script setup>
 const onSubmit = () => {
   console.log('sent')
+}
+
+import { ref } from 'vue'
+
+const theme = ref('light')
+
+function onClick () {
+  theme.value = theme.value === 'light' ? 'dark' : 'light'
 }
 </script>
 
