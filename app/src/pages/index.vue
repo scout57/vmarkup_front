157305238862cb9105e84a8e459861f8a4a8de00
Алексей Король <a.korol@applegalhelp.ru>
2024-09-28 03:52:14 +0300

    v2

--- app/src/pages/index.vue
+++ app/src/pages/index.vue
@@ -1,7 +1,78 @@
 <template>
-  <HelloWorld />
-</template>
+  <v-card class="mx-auto px-4 py-4" max-width="600" title="Результат поиска" subtitle="Список видео подходящих под условия">
 
-<script lang="ts" setup>
-  //
+    <v-table
+      fixed-header
+    >
+      <thead>
+      <tr>
+        <th class="text-left">
+          ID
+        </th>
+        <th class="text-left">
+          Описание
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr
+        v-for="item in desserts"
+        :key="item.name"
+      >
+        <td>{{ item.name }}</td>
+        <td>{{ item.calories }}</td>
+      </tr>
+      </tbody>
+    </v-table>
+  </v-card>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      desserts: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+        },
+        {
+          name: 'Gingerbread',
+          calories: 356,
+        },
+        {
+          name: 'Jelly bean',
+          calories: 375,
+        },
+        {
+          name: 'Lollipop',
+          calories: 392,
+        },
+        {
+          name: 'Honeycomb',
+          calories: 408,
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+        },
+      ],
+    }
+  },
+}
 </script>
