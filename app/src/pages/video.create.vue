39baf002d6e8ba87606bf57af80c15233a4b5572
Алексей Король <a.korol@applegalhelp.ru>
2024-09-28 03:09:58 +0300

    v2

--- app/src/pages/video.create.vue
+++ app/src/pages/video.create.vue
@@ -1,7 +1,47 @@
 <template>
-  <div>hello!</div>
+  <v-sheet class="bg-deep-purple pa-12 h-screen" rounded>
+    <v-card class="mx-auto px-6 py-6" max-width="600" title="Добавление видео">
+      <v-form
+        v-model="form"
+        @submit.prevent="onSubmit"
+      >
+
+        <v-file-input
+          class="py-6"
+          label="Файл"
+          show-size
+          v-model="file"
+          variant="outlined"
+        ></v-file-input>
+
+        <v-btn
+          :disabled="!form"
+          :loading="loading"
+          color="success"
+          size="large"
+          type="submit"
+          variant="elevated"
+          block
+        >
+          Загрузить
+        </v-btn>
+      </v-form>
+    </v-card>
+  </v-sheet>
 </template>
 
-<script lang="ts" setup>
-  //
+
+<script setup>
+function onSubmit() {
+  console.log('sent')
+}
+</script>
+
+<script>
+export default {
+  data: () => ({
+    form: false,
+    file: '',
+  }),
+}
 </script>
